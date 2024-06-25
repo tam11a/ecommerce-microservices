0dec5b1f5be92f0cb5011fa179537067b3add18b
tam11a <ibrahimsadiktamim@gmail.com>
2024-06-25 21:06:41 +0600

    a clean start

--- apps/inventory/src/inventory.module.ts
+++ apps/inventory/src/inventory.module.ts
@@ -1,20 +1,18 @@
 import { Module } from '@nestjs/common';
-import { InventoryController } from './inventory.controller';
-import { InventoryService } from './inventory.service';
-
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: {
-        federation: 2,
-      },
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      typePaths: ['**/*.graphql'],
     }),
   ],
-  controllers: [InventoryController],
-  providers: [InventoryService],
+  controllers: [],
+  providers: [],
 })
 export class InventoryModule {}
--- apps/inventory/src/inventory.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class InventoryService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
